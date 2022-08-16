8003c6a1ff9482a2ff748c363e8af21df55e136f
Angelo Fondello <angelofondello@gmail.com>
2022-08-16 00:03:40 +0000

    V1 Desafio 2

---
+++ Desafio2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+echo "Atualizando Servidor..."
+apt-get update -y
+apt-get upgrade -y
+
+echo "Instalando Arquivos..."
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Baixando Arquivos 'linux-site-dio'..."
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+
+echo "Descompactando Arquivos..."
+unzip main.zip
+
+echo "Organizando arquivos..."
+cd linux-site-dio
+cp -r* /var/www/html/
+
